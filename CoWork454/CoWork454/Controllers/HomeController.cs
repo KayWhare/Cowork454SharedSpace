6c4bb9e0954fbce4390aef31d80f9e490320962f
Dfrench2 <davidfrenchy89@gmail.com>
2020-05-06 16:11:01 +0800

    Asp.net framework

---
+++ CoWork454/CoWork454/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoWork454.Models;
+
+namespace CoWork454.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+}
