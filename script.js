5053b0225ca7dc470dacc0275392673adfb016b3
kayWhare <k_whare@hotmail.com>
2020-05-08 12:58:09 +0800

    no message

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
-// // -----------NAV TOGGLE-------------------
+// // -----------TOGGLE BUTTONS FOR NAV-------------------
 
 const hamburger = document.getElementById('slideNavToggle');
 let slideNav = document.querySelector('.slide-nav');
+let loginSlide = document.getElementById('login');
+let loginBtns = document.querySelectorAll('.members-login');
+var contactForm = document.getElementById('contactForm');
 
 hamburger.addEventListener('click', function(){
-   if (hamburger.checked == true){
-      return slideNav.style.height = '380px';
+   if (hamburger.checked == true ||
+      contactForm.style.height == '600px' ||
+      loginSlide.style.height == '70px'
+      ){
+         contactForm.style.height = '0px';   
+         loginSlide.style.height = '0px';
+         slideNav.style.height = '380px';
    }else{
     slideNav.style.height = '0px';
    }
 });
+function loginToggle(){
+      if (loginSlide.style.height == '0px' ||
+         hamburger.checked == true ||
+         contactForm.style.height == '600px'){
+            hamburger.checked = false;
+            slideNav.style.height = '0px';
+            contactForm.style.height = '0px';
+            navHeight()   
+      }else{
+         loginSlide.style.height = '0px';
+      }
+   };
+   function navHeight(){
+      if(window.innerWidth >= 576){
+         loginSlide.style.height = '60px';
+      }else{
+         loginSlide.style.height = '180px';
+      }
+    };
+   window.addEventListener('resize', ()=>{
+      if(loginSlide.style.height > '0px'){
+         navHeight();
+      }
+   });
+function contactToggle(){
+   var contactForm = document.getElementById('contactForm');
+   if(contactForm.style.height == '0px' ||
+      loginSlide.style.height == '70px' ||
+      hamburger.checked == true){
+         hamburger.checked = false;
+         loginSlide.style.height = '0px';
+         slideNav.style.height = '0px';
+         contactForm.style.height = '600px';
+   }else{
+      contactForm.style.height = '0px';
+   }
+}
+function navHeight(){
+   if(window.innerWidth >= 576){
+      loginSlide.style.height = '60px';
+   }else{
+      loginSlide.style.height = '180px';
+   }
+ };
 
-let loginBtn = document.getElementById('loginMobileView');
-let fullNav = document.querySelector('.fullwidth-nav');
-let topLogin = document.querySelectorAll('.members-login')[0];
-let navLogo = document.querySelector('.nav-logo');
-
-// window.addEventListener('scroll', ()=>{
-//    var fullnavTop = fullNav.scrollTop = 150;
-//    if(window.pageYOffset > fullnavTop ){
-//       loginBtn.style.display = 'flex';
-//       navLogo.style.display = 'flex';
-//    }else if(window.pageYOffset < 120 &&
-//       window.innerWidth > 992){
-//       loginBtn.style.display = 'none';
-//    // }else if(topLogin.style.display = 'flex' &&
-//    //    window.innerWidth > 768) {
-//    //    loginBtn.style.display = 'none';
-//    // }else if(window.innerWidth < 768){
-//    //    loginBtn.style.display = 'flex';
-//    //    loginBtn.style.marginRight = '60px'
-   //       }
-   // });
 
