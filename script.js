ca2562a615fd0aa842d78939bf7f71f8c8d401b7
kayWhare <k_whare@hotmail.com>
2020-05-08 15:06:53 +0800

    Merge branch 'master' of https://github.com/CodemasterInstitute/CoWork454Team1
    # Conflicts:
    #	script.js

--- script.js
+++ script.js
@@ -63,4 +63,62 @@ function navHeight(){
    }
  };
 
+<<<<<<< HEAD
+=======
+let loginBtn = document.getElementById('loginMobileView');
+let fullNav = document.querySelector('.fullwidth-nav');
+let topLogin = document.querySelectorAll('.members-login')[0];
+let navLogo = document.querySelector('.nav-logo');
+
+
+window.onscroll = function() { scollFunc()};
+
+function scollFunc() {
+
+   const yTop = document.documentElement.scrollTop;
+
+   const topimg = document.getElementById("top-img");
+   const topbox = document.getElementById("top-box");
+
+   const midimg = document.getElementById("mid-img");
+   const midbox = document.getElementById("mid-box");
+
+   const botimg = document.getElementById("bot-img");
+   const botbox = document.getElementById("bot-box");
+
+ if (yTop > 600) {
+    topimg.classList.add("right-affect");
+    topbox.classList.add("left-affect");
+ }
+
+ if (yTop>1000){
+   midimg.classList.add("left-affect");
+   midbox.classList.add("right-affect");
+ }
+
+ if(yTop>1400){
+   botimg.classList.add("right-affect");
+   botbox.classList.add("left-affect");
+ }
+
+}
+
+
+// window.addEventListener('scroll', ()=>{
+//    var fullnavTop = fullNav.scrollTop = 150;
+//    if(window.pageYOffset > fullnavTop ){
+//       loginBtn.style.display = 'flex';
+//       navLogo.style.display = 'flex';
+//    }else if(window.pageYOffset < 120 &&
+//       window.innerWidth > 992){
+//       loginBtn.style.display = 'none';
+//    // }else if(topLogin.style.display = 'flex' &&
+//    //    window.innerWidth > 768) {
+//    //    loginBtn.style.display = 'none';
+//    // }else if(window.innerWidth < 768){
+//    //    loginBtn.style.display = 'flex';
+//    //    loginBtn.style.marginRight = '60px'
+   //       }
+   // });
+>>>>>>> 1865941cfd487814d9b285dd30e3eac99cec6d26
 
