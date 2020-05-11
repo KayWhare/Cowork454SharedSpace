7f8860e827db9e82ea588e29f8b5f9ded3538ec0
Griffin_Jones <jonesg873@gmail.com>
2020-05-11 14:08:24 +0800

    Added Register Page

---
+++ CoWork454/CoWork454/Common/DatePickerHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoWork454.Common
+{
+    public class DatePickerHelper
+    {
+        //Set the Start Time Value
+        public static DateTime start = DateTime.ParseExact("07:00", "HH:mm", null);
+        //Set the End Time Value
+        public static  DateTime end = DateTime.ParseExact("15:00", "HH:mm", null);
+        //Set the interval time 
+        public static int interval = 15;
+
+        public static List<string> GenerateIntervals()
+        {
+            //List to hold the values of intervals
+            List<string> lstTimeIntervals = new List<string>();
+            //Populate the list with 30 minutes interval values
+            for (DateTime i = start; i <= end; i = i.AddMinutes(interval))
+                lstTimeIntervals.Add(i.ToString("HH:mm tt"));
+
+            return lstTimeIntervals;
+        }
+
+        //https://forums.asp.net/t/1879928.aspx?Drop+down+list+to+display+hours+and+minutes
+        public List<DateTime> GetTimeIntervals(DateTime start, DateTime end, TimeSpan interval)
+        {
+            //Your list of intervals
+            List<DateTime> intervals = new List<DateTime>();
+
+            //Rounds your start time to the nearest Interval
+            start = RoundUp(start, interval);
+
+            //Begin incrementing until you have reached your end Date
+            while (start <= end)
+            {
+                intervals.Add(start);
+                start = start.Add(interval);
+            }
+            //Returns your Intervals
+            return intervals;
+        }
+
+        //Useful function to round a DateTime to the nearest specified interval
+        DateTime RoundUp(DateTime dt, TimeSpan d)
+        {
+            return new DateTime(((dt.Ticks + d.Ticks - 1) / d.Ticks) * d.Ticks);
+        }
+    }
+}
--- CoWork454/CoWork454/Controllers/LoginController.cs
+++ CoWork454/CoWork454/Controllers/LoginController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using CoWork454.Data;
 using CoWork454.Models;
 using CoWork454.Controler;
+using CoWork454.Common;
 
 namespace CoWork454.Controllers
 {
@@ -40,10 +41,8 @@ namespace CoWork454.Controllers
             }
 
             // validate the incoming password with the password hash in the database
-            //var passwordVerified = BCrypt.Net.BCrypt.Verify(model.Pass, existingUser.PasswordHash);
-            var passwordVerified = _CoWork454Context.User.SingleOrDefault(l => l.PasswordHash == model.Password);
-            //replace above
-            if (passwordVerified == null)
+            var passwordVerified = BCrypt.Net.BCrypt.Verify(model.Password, existingUser.PasswordHash);
+            if (!passwordVerified)
             {
                 ModelState.AddModelError("UserName", "Either the user name or password was incorrect");
                 return View();
@@ -67,5 +66,90 @@ namespace CoWork454.Controllers
 
         }
 
+
+        //register
+        public IActionResult Register()
+        {
+            //only available if logged in
+            //gets user cookie if already logged in
+            var userIdCookie = GetEncryptedUserCookie("USER_ID");
+
+            if (userIdCookie == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+
+        // Register submit view
+        // [POST] /Login/Register
+        [HttpPost]
+        public IActionResult Register(Register model)
+        {
+
+            //only available if logged in
+            var userIdCookie = GetEncryptedUserCookie("USER_ID");
+            if (userIdCookie == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            // check if email already exists in database (if so throw exception)
+            var existingUser = _CoWork454Context.User.SingleOrDefault(u => u.Email == model.Email);
+
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("UserName", "UserName already exists in database");
+                return View();
+            }
+
+            // hash the incoming password
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(model.Password);
+
+            // create a new user record
+            var User = new User();
+            User.FirstName = model.FirstName;
+            User.LastName = model.LastName;
+            User.Email = model.Email;
+            User.CompanyName = model.CompanyName;
+            User.Phone = model.Phone;
+            User.PasswordHash = passwordHash;
+
+
+            try
+            {
+                _CoWork454Context.User.Add(User);
+                _CoWork454Context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View();
+            }
+
+            // redirect to the login
+            return RedirectToAction("Index");
+        }
+
+        // /User/Logout
+        public IActionResult Logout()
+        {
+            HttpContext.Response.Cookies.Delete("USER_ID");
+            return RedirectToAction("Index", "Home");
+        }
+
+        protected string GetEncryptedUserCookie(string cookieKey)
+        {
+            var encryptedShopCookie = HttpContext.Request.Cookies[cookieKey];
+
+            if (encryptedShopCookie == null)
+            {
+                return null;
+            }
+
+            var cookieContent = EncryptionHelper.DecryptString(encryptedShopCookie, EncryptionHelper.EncryptionKey);
+
+            return cookieContent;
+        }
     }
 }
