d331bfee68b70e367ee954321bb2fb7920b86753
Griffin_Jones <jonesg873@gmail.com>
2020-05-12 15:57:22 +0800

    Added view data check in home controler

--- CoWork454/CoWork454/Controllers/HomeController.cs
+++ CoWork454/CoWork454/Controllers/HomeController.cs
@@ -6,17 +6,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CoWork454.Models;
-
+using CoWork454.Common;
+using CoWork454.Data;
 
 namespace CoWork454.Controllers
 {
     public class HomeController : Controller
     {
 
-       
+
+        private readonly CoWork454Context _CoWork454Context;
+
+        public HomeController(CoWork454Context tastingsContext)
+        {
+            _CoWork454Context = tastingsContext;
+        }
+
+
 
         public IActionResult Index()
         {
+            var userIdCookie = GetEncryptedUserCookie("USER_ID");
+            if (userIdCookie != null)
+            {
+                var existingUser = _CoWork454Context.User.SingleOrDefault(l => l.Id == Convert.ToInt32(userIdCookie));
+                ViewData["User"] = existingUser;
+
+            }
             return View();
         }
 
@@ -30,6 +46,20 @@ namespace CoWork454.Controllers
             return View();
         }
 
+
+        protected string GetEncryptedUserCookie(string cookieKey)
+        {
+            var encryptedShopCookie = HttpContext.Request.Cookies[cookieKey];
+
+            if (encryptedShopCookie == null)
+            {
+                return null;
+            }
+
+            var cookieContent = EncryptionHelper.DecryptString(encryptedShopCookie, EncryptionHelper.EncryptionKey);
+
+            return cookieContent;
+        }
 
     }
 
