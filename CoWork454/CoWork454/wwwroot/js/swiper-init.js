5792533993eb9319f9a3b562e2b882dd1fad2ff4
David <davidfrenchy89@gmail.com>
2020-05-13 11:36:05 +0800

    Added Spaces to ASP

---
+++ CoWork454/CoWork454/wwwroot/js/swiper-init.js
@@ -0,0 +1,82 @@
+
+var galleryThumbs = new Swiper('.gallery-thumbs-one', {
+    spaceBetween: 5,
+    slidesPerView: 5,
+
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    setWrapperSize: true,
+  });
+
+
+
+  var galleryTop = new Swiper('.gallery-top-one', {
+    spaceBetween: 5,
+    loop:false,
+    loopedSlides: 5, //looped slides should be the same
+    setWrapperSize: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  });
+
+
+
+  var galleryThumbs = new Swiper('.gallery-thumbs-two', {
+    spaceBetween: 5,
+    slidesPerView: 5,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    setWrapperSize: true,
+  });
+
+
+
+  var galleryTop = new Swiper('.gallery-top-two', {
+    spaceBetween: 5,
+    loop:false,
+    loopedSlides: 5, //looped slides should be the same
+    setWrapperSize: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  });
+
+  var galleryThumbs = new Swiper('.gallery-thumbs-three', {
+    spaceBetween: 5,
+    slidesPerView: 5,
+
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    setWrapperSize: true,
+  });
+
+
+
+  var galleryTop = new Swiper('.gallery-top-three', {
+    spaceBetween: 5,
+    loop:false,
+    loopedSlides: 5, //looped slides should be the same
+    setWrapperSize: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  });
