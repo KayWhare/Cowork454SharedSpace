39f45f177a380352fad2e86df47a54f422260e67
Griffin_Jones <jonesg873@gmail.com>
2020-05-13 11:10:32 +0800

    Added empty admin page

---
+++ CoWork454/CoWork454/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoWork454.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoWork454.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var userIdCookie = GetEncryptedUserCookie("ADMIN");
+            if (userIdCookie != null)
+            {
+                return View();
+
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+        protected string GetEncryptedUserCookie(string cookieKey)
+        {
+            var encryptedShopCookie = HttpContext.Request.Cookies[cookieKey];
+
+            if (encryptedShopCookie == null)
+            {
+                return null;
+            }
+
+            var cookieContent = EncryptionHelper.DecryptString(encryptedShopCookie, EncryptionHelper.EncryptionKey);
+
+            return cookieContent;
+        }
+    }
+}
