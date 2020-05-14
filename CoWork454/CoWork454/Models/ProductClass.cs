1e5dce4248091c0140309e28012c429aefe25ed2
Griffin_Jones <jonesg873@gmail.com>
2020-05-14 14:43:52 +0800

    Added product class

---
+++ CoWork454/CoWork454/Models/ProductClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoWork454.Models
+{
+    public enum ProductClass
+    {
+        Workstation = 0,
+        Office = 1,
+        MeetingRoom = 2,
+        BoardRoom = 3,
+        TrainingRoom = 4,
+        QuietRoom = 5
+    }
+}
